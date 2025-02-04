5399b684b3082c5226df5e1a8f4cfde2b8f34644
AdityaGupta <60668516+adityagithubgupta@users.noreply.github.com>
2025-02-04 22:15:04 +0530

    Add files via upload

--- SendEmail.php
+++ SendEmail.php
@@ -1,9 +1,11 @@
+<?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $to = "guptaditya.1993@gmail.com";
+    $to = "guptaditya.1993@gmail.com"; 
     $subject = "New Contact Form Submission";
     
     $name = htmlspecialchars($_POST["name"]);
     $email = htmlspecialchars($_POST["email"]);
+    $organization = !empty($_POST["organization"]) ? htmlspecialchars($_POST["organization"]) : "Not provided";
     $message = htmlspecialchars($_POST["message"]);
     
     $headers = "From: $email\r\n";
@@ -13,6 +15,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $emailBody = "You have received a new message:\n\n";
     $emailBody .= "Name: $name\n";
     $emailBody .= "Email: $email\n";
+    $emailBody .= "Organization: $organization\n";
     $emailBody .= "Message:\n$message\n";
 
     if (mail($to, $subject, $emailBody, $headers)) {
@@ -21,3 +24,4 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         echo "error";
     }
 }
+?>
